2060e0f3bccee2f7f6949b97657e7a6e4aa9261a
sacummings91 <sacummings91@gmail.com>
2019-03-28 20:03:05 -0700

    Initialize Repository

---
+++ .prettierrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+  arrowParens: 'avoid',
+  bracketSpacing: true,
+  jsxBracketSameLine: true,
+  parser: 'babel',
+  printWidth: 80,
+  semi: true,
+  singleQuote: true,
+  tabWidth: 2,
+  trailingComma: 'none',
+  useTabs: false
+};
